8d232bec6311777fa8076f7fbfc269b08eb1ac55
NFahmid <149222168+NFahmid@users.noreply.github.com>
2024-10-12 01:00:07 +0600

    rough ui

---
+++ src/DonationHistory.java
@@ -0,0 +1,16 @@
+class DonationHistory {
+    private Donor donor;
+    private String donationType;
+//    private Date donationDate;
+
+    // Methods: checkCooldown, updateHistory, etc.
+
+    public boolean checkCooldown() {
+        // Implementation
+        return false;
+    }
+
+    public void updateHistory() {
+        // Implementation
+    }
+}
