289560ee4327f2f923d927175010ef926d90c584
MahadiAhmed0 <mahadiahmed.2002@gmail.com>
2024-11-09 21:46:37 +0600

    fixed main clear

--- src/Code/Main.java
+++ src/Code/Main.java
@@ -1,8 +1,11 @@
 package Code;
+import UI.ConsoleUtils;
 import UI.Login_UI;
 
 public class Main {
     public static void main(String[] args) {
+        ConsoleUtils consoleUtils = new ConsoleUtils();
+        consoleUtils.clearScreen();
         Login_UI.main(args);
     }
 }
